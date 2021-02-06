412b44263063fdfa803d264e9ab1819b2968d5ac
Sam Mudick <smudick@gmail.com>
2021-02-06 11:18:01 -0600

    corporate classes

---
+++ CorporateClass/Company.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorporateClass
+{
+    public class Company
+    {
+        // Some properties
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a private property for holding a collection of current employees
+        List<string> Employees { get; set; } = new List<string>();
+        // Create a method that allows a caller to add an employee
+        public void Hire(string name)
+        {
+            Employees.Add(name);
+        }
+        // Create a method that allows a caller to remove an employee
+        public void Fire(string name)
+        {
+            if (Employees.Contains(name))
+            {
+                Employees.Remove(name);
+            }
+            else
+            {
+                Console.WriteLine($"{name} doesn't even work here");
+            }
+        }
+        // Create a method that allows a caller to retrieve the list of employees
+
+        public void GetEmployees()
+        {
+            Console.WriteLine($"{Name} employee roster\n-------------------");
+            foreach (var employee in Employees)
+            {
+                Console.WriteLine(employee);
+            }
+            Console.WriteLine("-------------------\n");
+        }
+
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+
+            The constructor will set the value of the public properties
+        */
+        public Company(string name, DateTime createdOn)
+        {
+            Name = name;
+            CreatedOn = createdOn;
+        }
+    }
+}
---
+++ CorporateClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CorporateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Google = new Company("Google", new DateTime(1995, 1, 13));
+
+            Google.Hire("Sam");
+            Google.Hire("Kaitlyn");
+            Google.Hire("Dana");
+            Google.Hire("Bob the worst employee ever");
+
+            Google.GetEmployees();
+
+            Google.Fire("Bob the worst employee ever");
+            Google.GetEmployees();
+        }
+    }
+}
